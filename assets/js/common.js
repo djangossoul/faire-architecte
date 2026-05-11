5b41af16f1e51dec50cce692d85ab9f1ca2c9ae2
djangossoul <guillaume@kowabonga.co>
2026-05-11 16:17:07 +0200

    Fixes for iPhone p10

--- assets/js/common.js
+++ assets/js/common.js
@@ -83,9 +83,10 @@ Function Scroll Effects
 				}
 				transformDelta(delta) {
 					const { limit, offset, } = this.scrollbar;
+					const x = this._remainMomentum.x + delta.x;
 					const y = this._remainMomentum.y + delta.y;
-					// clamps momentum within [-offset, limit - offset] — x locked to 0 to prevent horizontal scroll
-					this.scrollbar.setMomentum(0, Math.max(-offset.y, Math.min(y, limit.y - offset.y)));
+					// clamps momentum within [-offset, limit - offset]
+					this.scrollbar.setMomentum(Math.max(-offset.x, Math.min(x, limit.x - offset.x)), Math.max(-offset.y, Math.min(y, limit.y - offset.y)));
 					return { x: 0, y: 0 };
 				}
 				onRender(remainMomentum) {
